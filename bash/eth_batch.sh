fe57e646982daee72504a0488d8a8a36ff86794d
Dan Saunders <djsaunde@umass.edu>
2018-02-05 16:22:27 -0500

    Fixing bug in batch script.

--- bash/eth_batch.sh
+++ bash/eth_batch.sh
@@ -1,17 +1,17 @@
 # Dispatch several jobs on the UMass CICS Gypsum cluster.
-for seed in 1 2 3
+for seed in 1
 do
-	for n_neurons in 625
+	for n_neurons in 100 225 400 625
 	do
 		for n_train in 60000
 		do
-			for c_inhib in -15.0 -17.5 -20.0 -22.5
+			for c_excite in 20.0 22.5 25.0
 			do
-				for c_excite in 20.0 22.5 25.0
+				for c_inhib in 15.0 17.5 20.0 22.5
 				do
 					for wmax in 0.75 1.0 1.25
 					do
-						sbatch eth.sh $seed $n_neurons $n_train $c_excite $excite $wmax
+						sbatch eth.sh $seed $n_neurons $n_train $c_excite $c_inhib $wmax
 					done		
 				done
 			done
